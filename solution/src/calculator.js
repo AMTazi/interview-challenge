ffd232a54e5fa3d1cd38355ba0e05c3c0741ba90
AZZB <aikidocombat3@hotmail.com>
2017-12-04 19:57:43 +0100

    cases when we can't make a basin

--- solution/src/calculator.js
+++ solution/src/calculator.js
@@ -1,7 +1,38 @@
 
 
 export default function calculator(input) {
+  if(!input || input.length < 1) return 0;
+
+  const length = input.length;
   let water_size = 0;
+
+  let i = 0;
+  let j = length-1;
+
+  /**
+    TODO: update the explanation!
+
+    We have two case when the shape can't make a basin
+
+    - First case -> when all bars matches this : the height of the left bar is less than the height of the right bar.
+    - Second case ->  when the first case didn't match but it matches this: the height of the right bar is less than the height of left bar till it reaches the cursor i
+  */
+
+  // First case
+  while(i < length && input[i] <= input[i+1]) {
+    i++;
+  }
+
+  if(i+1 === length) return water_size;
+
+  // Second case
+  while(j > i && input[j] <= input[j-1]) {
+    j--;
+  }
+
+  if(j === i) return water_size;
+
+  // Implementing what I've done in my paper
 
   return water_size;
 }
--- solution/src/calculator.test.js
+++ solution/src/calculator.test.js
@@ -1,6 +1,25 @@
 import calculator from './calculator'
 
 describe('Calculator', () => {
+
+  it('should return 0 when the input is null or empty array', () => {
+    let water_size = calculator(null)
+    expect(water_size).toEqual(0)
+
+    water_size = calculator([])
+    expect(water_size).toEqual(0)
+  })
+
+  it('should return 0 | it should match the first case in algorithm', () => {
+    const water_size = calculator([2, 3, 4, 5])
+    expect(water_size).toEqual(0)
+  })
+
+  it('should return 0 | it should match the second case in algorithm', () => {
+    const water_size = calculator([2, 3, 5, 4, 3, 1])
+    expect(water_size).toEqual(0)
+  })
+
   it('should return 17', () => {
     const water_size = calculator([2, 5, 1, 3, 1, 2, 1, 7, 7, 6])
     expect(water_size).toEqual(17)
